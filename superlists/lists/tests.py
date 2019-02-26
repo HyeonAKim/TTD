fba925234ce0f292de982cfc6b5b6f4d9283982e
hyeona kim <>
2019-02-26 22:02:19 +0900

    첫 단위 테스트와 url 맵핑 그리고 임시뷰

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
-class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+# from django.core.urlresolvers import resolve
+# 업그레이드 되면서 없어짐
+
+from django.urls import reverse
+from django.test import TestCase
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = reverse('/')
+        self.assertEqual(found.func, home_page)
